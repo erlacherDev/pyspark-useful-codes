6b637d4747dd651e46c0c1b2980f061bff2361fc
ErlacherDev <fielerlacher@gmail.com>
2019-12-25 15:43:15 -0300

    read positional data as a Class

--- useful_codes/__init__.py
+++ useful_codes/__init__.py
@@ -4,9 +4,12 @@ useful pyspark codes
 
 """
 
+from useful_codes.copy_data import CopyData
 from useful_codes.create_tables import CreateTable
 from useful_codes.generate_dataframe import GenerateDataFrame
+from useful_codes.mask_data import MaskData
+from useful_codes.read_positional_data import ReadPositional
 from useful_codes.unnest_columns import UnnestColumn
 from useful_codes.write_table import WriteTable
 
-__all__ = ['CreateTable', 'GenerateDataFrame', 'UnnestColumn', 'WriteTable']
+__all__ = ['CreateTable', 'GenerateDataFrame', 'UnnestColumn', 'WriteTable', 'CopyData', 'MaskData', 'ReadPositional']
--- useful_codes/read_positional_data.py
+++ useful_codes/read_positional_data.py
@@ -2,50 +2,79 @@ from pyspark.sql import DataFrame
 from pyspark.sql.functions import substring, col
 
 
-def read_positional(spark, df_or_path, position_list):
+def read_positional(spark, df_or_path, column_list):
     """
-    :param spark: SparkSession (default como spark)
-    :param df_or_path: DataFrame com a coluna 'value' a ser separada ou o diretorio do arquivo posicional
-    :param position_list: Lista ou tupla de parametros para cada coluna (na ordem):
-        [nome_coluna, tamanho_coluna, datatype_coluna (opcional)]
+    :param spark: SparkSession
+    :param df_or_path: DataFrame with the column 'value' that will be splited or the path of the positional file.
+    :param column_list: List ou Tuple for each column (must be in order):
+        [column_name, column_range, column_type (opcional)]
         Ex:
-        position_list = [('col_1', 5, 'string'), ('col_2', '10', 'int'), ('col_3', 5)
-    :return: Retorna o DataFrame com as colunas definidas pelo parametro position_list
+        column_list = [('col_1', 5, 'string'), ('col_2', '10', 'int'), ('col_3', 5)
+    :return: Return the DataFrame with the columns defined at column_list
     """
 
-    if not isinstance(position_list, list):
-        raise TypeError(
-            "'position_list' must be of type 'list'. Found: '{0}'".format(type(position_list)))
+    return ReadPositional(spark, df_or_path, column_list).run()
 
-    if isinstance(df_or_path, DataFrame):
-        positional_df = df_or_path
-    elif isinstance(df_or_path, str):
-        positional_df = spark.read.text(df_or_path)
-    else:
-        raise TypeError(
-            "df_or_path must be of type 'DataFrame' or 'str'. Found: {0}".format(type(df_or_path)))
 
-    index_start = 1
+class ReadPositional:
 
-    for values_in_tuple in position_list:
+    def __init__(self, spark, df_or_path, column_list):
+        self._spark = spark
+        self.df_or_path = df_or_path
+        self.column_list = column_list
+
+    def run(self):
+
+        self._check_positional_list()
+        positional_df = self._get_input_df()
+        index_start = 1
+
+        for values_in_tuple in self.column_list:  # (column_name, column_range, column_type)
+            positional_df = self._build_positional_columns(index_start, positional_df, values_in_tuple)
+            index_start += values_in_tuple[1]
+
+        return positional_df.drop('value')
+
+    @staticmethod
+    def _build_positional_columns(index_start, positional_df, values_in_tuple):
+
         if not isinstance(values_in_tuple, tuple) and not isinstance(values_in_tuple, list):
             raise TypeError(
-                "Values inside 'position_list' must be of type 'list' or 'tuple'. Found: '{0}'"
+                "Values inside 'column_list' must be of type 'list' or 'tuple'. Found: '{0}'"
                     .format(type(values_in_tuple)))
 
         if len(values_in_tuple) < 2 or len(values_in_tuple) > 3:
-            raise ValueError("Values inside 'position_list' must be have length of 2 or 3.")
+            raise ValueError("Values inside 'column_list' must be have length of 2 or 3.")
 
         column_name = values_in_tuple[0]
         column_range = int(values_in_tuple[1])
 
         positional_df = positional_df.withColumn(column_name, substring(col('value'), index_start, column_range))
-        index_start += values_in_tuple[1]
 
         if len(values_in_tuple) == 3:
             positional_df = positional_df.withColumn(column_name, col(column_name).cast(values_in_tuple[2]))
 
-    return positional_df.drop('value')
+        return positional_df
+
+    def _get_input_df(self):
+
+        if isinstance(self.df_or_path, DataFrame):
+            positional_df = self.df_or_path
+
+        elif isinstance(self.df_or_path, str):
+            positional_df = spark.read.text(self.df_or_path)
+
+        else:
+            raise TypeError(
+                "df_or_path must be of type 'DataFrame' or 'str'. Found: {0}".format(type(self.df_or_path)))
+
+        return positional_df
+
+    def _check_positional_list(self):
+
+        if not isinstance(self.column_list, list):
+            raise TypeError(
+                "'column_list' must be of type 'list'. Found: '{0}'".format(type(self.column_list)))
 
 
 if __name__ == '__main__':
